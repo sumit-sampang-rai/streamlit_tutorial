87486f69f68a4f48829923cfafe6579e68f0037e
Sumit Sampang Rai <4235912+sumit-sampang-rai@users.noreply.github.com>
2024-04-18 08:35:09 +0545

    Update 05_caching.py

--- 05_caching.py
+++ 05_caching.py
@@ -1,1 +1,14 @@
 # https://docs.streamlit.io/develop/concepts/architecture/caching
+
+import pandas as pd
+import streamlit as st
+
+@st.cache_data  # 👈 Add the caching decorator
+def load_data(url):
+    df = pd.read_csv(url)
+    return df
+
+df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")
+st.dataframe(df)
+
+st.button("Rerun")
